9618b265f274383ab00ac15cb16ec1e20b0871f8
ishika agarwal <ishikaagarwal22318@gmail.com>
2023-10-25 11:36:03 +0530

    deleted pages folder and added show listings

--- src/Auth/SignUp.jsx
+++ src/Auth/SignUp.jsx
@@ -2,24 +2,22 @@ import React from 'react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 // // import OAuth from '../components/OAuth';
-import { useAuth } from "../AuthContext/AuthContext";
-
+import { useAuth } from '../AuthContext/AuthContext';
 
 export default function SignUp() {
-    const [formData, setFormData] = useState({});
-    const [error, setError] = useState(null);
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const {signup} = useAuth();
-    const handleChange = (e) => {
-      setFormData({
-        ...formData,
-        [e.target.id]: e.target.value,
-      });
-      console.log("fj");
-    };
-
+  const { signup } = useAuth();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+    console.log('fj');
+  };
 
   // const handleSubmit = async (e) => {
   //   e.preventDefault();
@@ -51,7 +49,6 @@ export default function SignUp() {
 
   // };
 
-
   const handleSubmit = async (e) => {
     console.log(formData);
     // e.preventDefault();
@@ -77,43 +74,38 @@ export default function SignUp() {
       setError(error.message);
       console.log(error);
     }
-
-  }
-
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <div
-        
-        className="flex flex-col gap-4"
-      >
+      <div className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg"
           id="username"
-        onChange={handleChange}
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
-        onChange={handleChange}
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
-        onChange={handleChange}
+          onChange={handleChange}
         />
 
         <button
           // disabled={loading}
           onClick={handleSubmit}
-          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 text-center"
         >
           {/* {loading ? 'Loading...' : 'Sign Up'} */}
           Sign Up
@@ -124,9 +116,14 @@ export default function SignUp() {
           <hr class="border-gray-500" />
         </div>
 
-        <button class="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 ">
-          <img height="50" width="50" src = "https://www.outsystems.com/forge/DownloadResource.aspx?FileName=&ImageBinaryId=43951" alt="google btn" />
-          <span class = "ml-4">Login with Google</span>
+        <button class="bg-blue-400 text-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 ">
+          <img
+            height="50"
+            width="50"
+            src="https://www.outsystems.com/forge/DownloadResource.aspx?FileName=&ImageBinaryId=43951"
+            alt="google btn"
+          />
+          <span class="ml-4">Login with Google</span>
         </button>
 
         {/* <OAuth /> */}
@@ -134,7 +131,7 @@ export default function SignUp() {
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         <Link to={'/login'}>
-        <span className="text-blue-700">Sign in</span>
+          <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
       {/* {error && <p className="text-red-500 mt-5">{error}</p>} */}
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -222,7 +222,7 @@ export default function Profile() {
         />
         <button
           // disabled={loading}
-          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80 text-center"
         >
           Update
           {/* {loading ? 'Loading...' : 'Update'} */}
@@ -234,7 +234,6 @@ export default function Profile() {
           Create Listing
         </Link>
       </form>
-
       <div className="flex justify-between mt-5">
         <span
           // onClick={handleDeleteUser}
@@ -249,60 +248,63 @@ export default function Profile() {
           Sign out
         </span>
       </div>
-
       {/* <p className="text-red-700 mt-5">{error ? error : ''}</p> */}
       {/* <p className="text-green-700 mt-5">
         {updateSuccess ? 'User is updated successfully!' : ''}
       </p> */}
       <button
         // onClick={handleShowListings}
-        className="text-green-700 w-full"
+        className="text-green-700 w-full text-center"
       >
         Show Listings
       </button>
       {/* <p className="text-red-700 mt-5">
         {showListingsError ? 'Error showing listings' : ''}
       </p> */}
+      {/* {userListings && userListings.length > 0 && ( */}
+      <div className="flex flex-col gap-4">
+        <h1 className="text-center mt-7 text-2xl font-semibold">
+          Your Listings
+        </h1>
+        {/* {userListings.map((listing) => ( */}
+        <div
+          // key={listing._id}
+          className="border rounded-lg p-3 flex justify-between items-center gap-4"
+        >
+          <Link
+          // to={`/listing/${listing._id}`}
+          >
+            <img
+              // src={listing.imageUrls[0]}
+              alt="listing cover"
+              className="h-16 w-16 object-contain"
+            />
+          </Link>
+          <Link
+            className="text-slate-700 font-semibold  hover:underline truncate flex-1"
+            // to={`/listing/${listing._id}`}
+          >
+            <p>***Listing Name Here***</p>
+            {/* <p>{listing.name}</p> */}
+          </Link>
 
-      {/* {userListings && userListings.length > 0 && (
-        <div className="flex flex-col gap-4">
-          <h1 className="text-center mt-7 text-2xl font-semibold">
-            Your Listings
-          </h1>
-          {userListings.map((listing) => (
-            <div
-              key={listing._id}
-              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+          <div className="flex flex-col item-center">
+            <button
+              // onClick={() => handleListingDelete(listing._id)}
+              className="text-red-700 uppercase"
             >
-              <Link to={`/listing/${listing._id}`}>
-                <img
-                  src={listing.imageUrls[0]}
-                  alt="listing cover"
-                  className="h-16 w-16 object-contain"
-                />
-              </Link>
-              <Link
-                className="text-slate-700 font-semibold  hover:underline truncate flex-1"
-                to={`/listing/${listing._id}`}
-              >
-                <p>{listing.name}</p>
-              </Link>
-
-              <div className="flex flex-col item-center">
-                <button
-                  // onClick={() => handleListingDelete(listing._id)}
-                  className="text-red-700 uppercase"
-                >
-                  Delete
-                </button>
-                <Link to={`/update-listing/${listing._id}`}>
-                  <button className="text-green-700 uppercase">Edit</button>
-                </Link>
-              </div>
-            </div>
-          ))}
+              Delete
+            </button>
+            <Link
+            // to={`/update-listing/${listing._id}`}
+            >
+              <button className="text-green-700 uppercase">Edit</button>
+            </Link>
+          </div>
         </div>
-      )} */}
+        {/* ))}  */}
+      </div>
+      {/* )} */}
     </div>
   );
 }
