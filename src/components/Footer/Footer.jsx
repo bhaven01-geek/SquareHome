554b720e434e8b33322eb9d764db1a14d90c580d
Satyam <Satyams963@gmail.com>
2023-10-26 17:11:38 +0530

    footer edited

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,201 +1,120 @@
-import './footer.css';
+import "./footer.css";
 
 export default function Footer() {
   return (
     <div>
-        <footer className="footer">
+      <footer className="footer">
+        <div className="footer-top">
+          <div className="container">
+            <div className="footer-brand">
+              <a href="#" className="logo">
+                <i class="fa-solid fa-building-user"></i>
+                <b>Square Home</b>
+              </a>
 
-<div className="footer-top">
-  <div className="container">
+              <p className="section-text">
+                Square Home is the best place to find your next perfect place to
+                live. We have a wide range of properties for you to choose from.
+              </p>
+            </div>
 
-    <div className="footer-brand">
+            <div className="footer-link-box">
+              <ul className="footer-list">
+                <li className="logo">
+                  <p>
+                    <b>Services</b>
+                  </p>
+                </li>
+                <li>
+                  <a href="#" className="footer-link">
+                    Buy Property
+                  </a>
+                </li>
 
-      <a href="#" className="logo">
-      <i class="fa-solid fa-building-user"></i>
-      <b>Square Home</b>
-      </a>
+                <li>
+                  <a href="#" className="footer-link">
+                    Sell Property
+                  </a>
+                </li>
 
-      <p className="section-text">
-        Square Home is the best place to find your next perfect place to live.
-        We have a wide range of properties for you to choose from.
-      </p>
+                <li>
+                  <a href="#" className="footer-link">
+                    EMI Calculator
+                  </a>
+                </li>
+              </ul>
+              <div className="footer-list">
+                <ul className="social-list">
+                  <li>
+                    <a href="#" className="social-link">
+                      <i className="topIcon fa-brands fa-square-facebook"></i>
+                      <ion-icon name="logo-facebook"></ion-icon>
+                    </a>
+                  </li>
 
-      <ul className="contact-list">
+                  <li>
+                    <a href="#" className="social-link">
+                      <ion-icon name="logo-twitter"></ion-icon>
+                      <i className="topIcon fa-brands fa-square-twitter"></i>
+                    </a>
+                  </li>
 
-        <li>
-          <a href="#" className="contact-link">
-            <ion-icon name="location-outline"></ion-icon>
+                  <li>
+                    <a href="#" className="social-link">
+                      <ion-icon name="logo-linkedin"></ion-icon>
+                      <i className="topIcon fa-brands fa-square-pinterest"></i>
+                    </a>
+                  </li>
 
-            <address>West Malad, Mumbai, India</address>
-          </a>
-        </li>
+                  <li>
+                    <a href="#" className="social-link">
+                      <ion-icon name="logo-youtube"></ion-icon>
+                      <i className="topIcon fa-brands fa-instagram"></i>
+                    </a>
+                  </li>
+                </ul>
+                <br />
+                <ul className="contact-list">
+                  <li>
+                    <a href="#" className="contact-link">
+                      <ion-icon name="location-outline"></ion-icon>
 
-        <li>
-          <a href="tel:+0123456789" className="contact-link">
-            <ion-icon name="call-outline"></ion-icon>
+                      <address>West Malad, Mumbai, India</address>
+                    </a>
+                  </li>
 
-            <span>+0123-456789</span>
-          </a>
-        </li>
+                  <li>
+                    <a href="tel:+0123456789" className="contact-link">
+                      <ion-icon name="call-outline"></ion-icon>
 
-        <li>
-          <a href="mailto:contact@homeverse.com" className="contact-link">
-            <ion-icon name="mail-outline"></ion-icon>
+                      <span>+0123-456789</span>
+                    </a>
+                  </li>
 
-            <span>contact@squarehome.com</span>
-          </a>
-        </li>
+                  <li>
+                    <a
+                      href="mailto:contact@homeverse.com"
+                      className="contact-link"
+                    >
+                      <ion-icon name="mail-outline"></ion-icon>
 
-      </ul>
+                      <span>contact@squarehome.com</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
 
-      <ul className="social-list">
-
-        <li>
-          <a href="#" className="social-link">
-          <i className="topIcon fa-brands fa-square-facebook"></i>
-            <ion-icon name="logo-facebook"></ion-icon>
-          </a>
-        </li>
-
-        <li>
-          <a href="#" className="social-link">
-            <ion-icon name="logo-twitter"></ion-icon>
-            <i className="topIcon fa-brands fa-square-twitter"></i>
-          </a>
-        </li>
-
-        <li>
-          <a href="#" className="social-link">
-            <ion-icon name="logo-linkedin"></ion-icon>
-            <i className="topIcon fa-brands fa-square-pinterest"></i>
-          </a>
-        </li>
-
-        <li>
-          <a href="#" className="social-link">
-            <ion-icon name="logo-youtube"></ion-icon>
-            <i className="topIcon fa-brands fa-instagram"></i>
-          </a>
-        </li>
-
-      </ul>
-
+        <div className="footer-bottom">
+          <div className="container">
+            <p className="copyright">
+              &copy; 2023 <a href="#">Square Home</a>. All Rights Reserved
+            </p>
+          </div>
+        </div>
+      </footer>
     </div>
-
-    <div className="footer-link-box">
-
-      <ul className="footer-list">
-
-        <li>
-          <p className="footer-list-title">Company</p>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">About</a>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">Blog</a>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">All Products</a>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">Locations Map</a>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">FAQ</a>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">Contact us</a>
-        </li>
-
-      </ul>
-
-      <ul className="footer-list">
-
-        <li>
-          <p className="footer-list-title">Services</p>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">Order tracking</a>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">Wish List</a>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">Login</a>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">My account</a>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">Terms & Conditions</a>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">Promotional Offers</a>
-        </li>
-
-      </ul>
-
-      <ul className="footer-list">
-
-        <li>
-          <p className="footer-list-title">Customer Care</p>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">Login</a>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">My account</a>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">Wish List</a>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">Order tracking</a>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">FAQ</a>
-        </li>
-
-        <li>
-          <a href="#" className="footer-link">Contact us</a>
-        </li>
-
-      </ul>
-
-    </div>
-
-  </div>
-</div>
-
-<div className="footer-bottom">
-  <div className="container">
-
-    <p className="copyright">
-      &copy; 2023 <a href="#">Square Home</a>. All Rights Reserved
-    </p>
-
-  </div>
-</div>
-
-</footer>
-    </div>
-  )
+  );
 }
